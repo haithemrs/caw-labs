0f940bf0db8d96ea3657a23a54f5220e961fb2c3
Haithem <heithem.raouas@univ-constantine2.com>
2025-11-15 13:00:32 +0100

    Remove all files except Lab1 and README

--- fileImport.js
+++
@@ -1,5 +0,0 @@
-const mean = require('./notation');
-
-const arr = [10, 15, 12, 18, 20];
-
-console.log("Average =", mean(arr));
--- notation.js
+++
@@ -1,9 +0,0 @@
-function mean(scores) {
-    let sum = 0;
-    for (let s of scores) {
-        sum += s;
-    }
-    return sum / scores.length;
-}
-
-module.exports = mean;
